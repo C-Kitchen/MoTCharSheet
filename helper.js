d390740f68a3a2e6d7a3cd0439579a1e2c27c397
C-Kitchen <52185657+C-Kitchen@users.noreply.github.com>
2025-10-22 21:32:26 -0500

    attempt to handle touch

--- helper.js
+++ helper.js
@@ -70,7 +70,9 @@ class HarmTrack{
 			this.buttons[i].className = "trackbutton";
 			this.track.appendChild(this.buttons[i]);
 			this.buttons[i].addEventListener("mousedown",() => this.starthold(i));
-			this.buttons[i].addEventListener("mouseup",() => this.push(i));
+			this.buttons[i].addEventListener("touchstart",() => this.starthold(i));
+			this.buttons[i].addEventListener("click",() => this.push(i));
+			this.buttons[i].addEventListener("touchend",() => this.push(i));
 			this.buttons[i].addEventListener("mouseover",() => this.starthover(i));
 			this.buttons[i].addEventListener("mouseout",() => this.updateButtons());
 		}
